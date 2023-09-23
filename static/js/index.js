193082affbf6b9ac27363af1a0d26f02c0dc9ac2
Miroslav David <mirasira54123698745@gmail.com>
2023-09-23 20:20:24 +0200

    Sliding

---
+++ static/js/index.js
@@ -0,0 +1,13 @@
+document.querySelectorAll('.topnav a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+      e.preventDefault();
+  
+      const targetId = this.getAttribute('href').substring(1);
+      const targetElement = document.getElementById(targetId);
+  
+      window.scrollTo({
+        top: targetElement.offsetTop,
+        behavior: 'smooth'  // Smooth scroll
+      });
+    });
+  });
