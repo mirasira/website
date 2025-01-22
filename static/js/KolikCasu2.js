e598bec6dbc8f922f288ae28a436d2c5e9dbb723
Miroslav David <mirasira54123698745@gmail.com>
2025-01-22 21:13:11 +0100

    big update KolikCasu2

---
+++ static/js/KolikCasu2.js
@@ -0,0 +1,525 @@
+const jsonString = `[
+
+    {
+    "name": "Hemiceratoides hieroglyphica",
+    "url": "https://en.wikipedia.org/wiki/Hemiceratoides_hieroglyphica#:~:text=Hemiceratoides%20hieroglyphica%20is%20a%20moth,specialized%2C%20harpoon%2Dlike%20proboscis.",
+    "why_interesting": "Moths drinks tears of sleeping birds.",
+    "additional_urls": ""
+    },
+    {
+        "name": "Namaqua sandgrouse",
+        "url": "https://en.wikipedia.org/wiki/Namaqua_sandgrouse",
+        "why_interesting": "This bird can carry water in its feathers to its chicks.",
+        "additional_urls": ""
+    },
+    {
+        "name": "Ubre Blanca",
+        "url": "https://en.wikipedia.org/wiki/Ubre_Blanca",
+        "why_interesting": "Some very productive cow from cuba.",
+        "additional_urls": ""
+    },
+    {
+        "name": "Strophurus taenicauda",
+        "url": "https://en.wikipedia.org/wiki/Strophurus_taenicauda",
+        "why_interesting": "It can produce foul smelling chemical from its tail.",
+        "additional_urls": [
+            {
+                "name": "Video",
+                "url": "https://www.youtube.com/watch?v=sl0Ztj7Q70Y"
+            }
+        ]
+    },
+    {
+        "name": "Rat-tailed maggot",
+        "url": "https://en.wikipedia.org/wiki/Rat-tailed_maggot",
+        "why_interesting": "Just did not know that this exists.",
+        "additional_urls": ""
+    },
+    {
+        "name": "Kuru",
+        "url": "https://cs.wikipedia.org/wiki/Kuru",
+        "why_interesting": "Eating human brains can be dangerous.",
+        "additional_urls": ""
+    },
+    {
+        "name": "Triboluminescence",
+        "url": "https://en.wikipedia.org/wiki/Triboluminescence",
+        "why_interesting": "Sugar can glow when crushed?",
+        "additional_urls": ""
+    },
+    {
+        "name": "Pythium oligandrum",
+        "url": "https://cs.wikipedia.org/wiki/Pythium_oligandrum",
+        "why_interesting": "Fungi that kills othe fungi and thats like everything it does.",
+        "additional_urls": ""
+    },
+    {
+        "name": "Japanese folktales",
+        "url": "https://en.wikipedia.org/wiki/Japanese_folktales",
+        "why_interesting": "Why is it there?",
+        "additional_urls": ""
+    },
+    {
+        "name": "Crushing (execution)",
+        "url": "https://en.wikipedia.org/wiki/Crushing_(execution)",
+        "why_interesting": "Did not know that this was a thing.",
+        "additional_urls": ""
+    },
+    {
+        "name": "Shakespeare (programovací jazyk)",
+        "url": "https://cs.wikipedia.org/wiki/Shakespeare_(programovac%C3%AD_jazyk)",
+        "why_interesting": "Very cool programming language.",
+        "additional_urls": ""
+    },
+    {
+        "name": "Fu-Go balloon bomb",
+        "url": "https://en.wikipedia.org/wiki/Fu-Go_balloon_bomb",
+        "why_interesting": "Japan uses these to atack mainland USA.",
+        "additional_urls": ""
+    },
+    {
+        "name": "Taikyoku shogi",
+        "url": "https://en.wikipedia.org/wiki/Taikyoku_shogi#:~:text=Taikyoku%20sh%C5%8Dgi%20(Japanese%3A%20%E5%A4%A7%E5%B1%80%E5%B0%86%E6%A3%8B,earlier%20large%20board%20shogi%20games.",
+        "why_interesting": "Very, very big shogi",
+        "additional_urls": ""
+    },
+    {
+        "name": "Ezoterický programovací jazyk",
+        "url": "https://cs.wikipedia.org/wiki/Ezoterick%C3%BD_programovac%C3%AD_jazyk#OstraJAVA",
+        "why_interesting": "Cool programming languages",
+        "additional_urls": [
+            {
+                "name": "List of esoteric programming languages",
+                "url": "https://esolangs.org/wiki/Language_list"
+            }
+        
+        ]
+    },
+    {
+        "name": "Adrian Carton de Wiart",
+        "url": "https://en.wikipedia.org/wiki/Adrian_Carton_de_Wiart",
+        "why_interesting": "Kinda based guy. He sustained many injuries and after the war he said that he enjoyed the war.",
+        "additional_urls": ""
+    },
+    {
+        "name": "Shepherd's leap",
+        "url": "https://en.wikipedia.org/wiki/Shepherd%27s_leap",
+        "why_interesting": "Interesting mean of transport",
+        "additional_urls": ""
+    },
+    {
+        "name": "Adenotrophic viviparity",
+        "url": "https://en.wikipedia.org/wiki/Adenotrophic_viviparity",
+        "why_interesting": "Tse tse flys, 'kloši' and some other insects give birth to larvae.",
+        "additional_urls": ""
+    },
+    {
+        "name": "Dahala Khagrabari",
+        "url": "https://en.wikipedia.org/wiki/Dahala_Khagrabari",
+        "why_interesting": "Piece of India within a piece of Bangladesh within a piece of India within Bangladesh.",
+        "additional_urls": ""
+    },
+    {
+        "name": "Shirime",
+        "url": "https://en.m.wikipedia.org/wiki/Shirime",
+        "why_interesting": "Some very interesting japanese creature.",
+        "additional_urls": ""
+    },
+    {
+        "name": "Mystacinobia zelandica",
+        "url": "https://cs.wikipedia.org/wiki/Mystacinobia_zelandica",
+        "why_interesting": "This is a fly that lives on bats and have very interesting symbiotic relationship with them.",
+        "additional_urls": ""
+    },
+    {
+        "name": "Hydnora",
+        "url": "https://cs.wikipedia.org/wiki/Hydnora",
+        "why_interesting": "Looks interesting, right? anyway, it is a parasitic plant. It does not have chlorophyll.",
+        "additional_urls": ""
+    },
+    {
+        "name": "Rúp Kund",
+        "url": "https://cs.wikipedia.org/wiki/R%C3%BAp_Kund",
+        "why_interesting": "Lake high in the himalayas. Big amount of skeletons were found here. ",
+        "additional_urls": ""
+    },
+    {
+        "name": "Portia (rod)",
+        "url": "https://cs.wikipedia.org/wiki/Portia_(rod)",
+        "why_interesting": "Super cool jumping spiders.",
+        "additional_urls": ""
+    },
+    {
+        "name": "Kloš jelení",
+        "url": "https://cs.wikipedia.org/wiki/Klo%C5%A1_jelen%C3%AD",
+        "why_interesting": "It gives birth to larvae.",
+        "additional_urls": ""
+    },
+    {
+        "name": "List of light sources",
+        "url": "https://en.wikipedia.org/wiki/List_of_light_sources",
+        "why_interesting": "Some of them are very interesting",
+        "additional_urls": ""
+    },
+    {
+        "name": "Common Surinam toad",
+        "url": "https://en.wikipedia.org/wiki/Common_Surinam_toad",
+        "why_interesting": "Looks funny. Also it has very interesting way of reproduction. Female carries eggs on her back.",
+        "additional_urls": ""
+    },
+    {
+        "name": "Neoteny in humans",
+        "url": "https://en.wikipedia.org/wiki/Neoteny_in_humans",
+        "why_interesting": "",
+        "additional_urls": ""
+    },
+    {
+        "name": "Wallace Line",
+        "url": "https://en.wikipedia.org/wiki/Wallace_Line",
+        "why_interesting": "",
+        "additional_urls": ""
+    },
+    {
+        "name": "Contest to kill 100 people using a sword",
+        "url": "https://en.wikipedia.org/wiki/Contest_to_kill_100_people_using_a_sword",
+        "why_interesting": "",
+        "additional_urls": ""
+    },
+    {
+        "name": "Toxic bird",
+        "url": "https://en.wikipedia.org/wiki/Toxic_bird",
+        "why_interesting": "These birds have toxic feathers with toxins same as in poison dart frogs",
+        "additional_urls": ""
+    },
+    {
+        "name": "Cecílie kroužkovaná",
+        "url": "https://cs.wikipedia.org/wiki/Cec%C3%ADlie_krou%C5%BEkovan%C3%A1",
+        "why_interesting": "It is Amphibian. It looks like a big worm. The mother cares for her young. She even feeds them. With her own skin!",
+        "additional_urls": ""
+    },
+    {
+        "name": "Channichthyidae",
+        "url": "https://en.wikipedia.org/wiki/Channichthyidae",
+        "why_interesting": "This fish doesnt have hemoglobin, so it has white blood!",
+        "additional_urls": ""
+    },
+    {
+        "name": "Euthalia aconthea",
+        "url": "https://en.wikipedia.org/wiki/Euthalia_aconthea",
+        "why_interesting": "The caterpillar looks cool",
+        "additional_urls": ""
+    },
+    {
+        "name": "Pláštěnci",
+        "url": "https://cs.wikipedia.org/wiki/Pl%C3%A1%C5%A1t%C4%9Bnci",
+        "why_interesting": "Their larvae are more complex than adults. E.g. they have chorda dorsalis. Also one of the first vertebrates on Earth",
+        "additional_urls": ""
+    },
+    {
+        "name": "Zavíječ voskový",
+        "url": "https://cs.wikipedia.org/wiki/Zav%C3%ADje%C4%8D_voskov%C3%BD",
+        "why_interesting": "It can eat plastics!",
+        "additional_urls": ""
+    },
+    {
+        "name": "Stromatolit",
+        "url": "https://cs.wikipedia.org/wiki/Stromatolit",
+        "why_interesting": "One of the oldest forms of life on Earth, still alive today. Similar form of life could be found on another planets",
+        "additional_urls": ""
+    },
+    {
+        "name": "Carbonate–silicate cycle",
+        "url": "https://en.wikipedia.org/wiki/Carbonate%E2%80%93silicate_cycle#:~:text=The%20carbonate%E2%80%93silicate%20geochemical%20cycle,rocks%20by%20metamorphism%20and%20volcanism.",
+        "why_interesting": "Just how carbon cycle works",
+        "additional_urls": ""
+    },
+    {
+        "name": "Operace Northwoods",
+        "url": "https://cs.wikipedia.org/wiki/Operace_Northwoods",
+        "why_interesting": "This is kinda crazy, really makes you think what else must have been inside job",
+        "additional_urls": ""
+    },
+    {
+        "name": "List of legendary creatures from Japan",
+        "url": "https://en.wikipedia.org/wiki/List_of_legendary_creatures_from_Japan",
+        "why_interesting": "Just interesting list of japanese creatures",
+        "additional_urls": ""
+    },
+    {
+        "name": "Dictamnus albus",
+        "url": "https://en.wikipedia.org/wiki/Dictamnus_albus",
+        "why_interesting": "This plant releases flammable oils which can be ignited in very hot weather. Also burning bush was in the Bible",
+        "additional_urls": ""
+    },
+    {
+        "name": "Mansfield Smith-Cumming",
+        "url": "https://en.wikipedia.org/wiki/Mansfield_Smith-Cumming",
+        "why_interesting": "His lastname is pretty self explanatory, didnt know it could be used as invisible ink",
+        "additional_urls": ""
+    },
+    {
+        "name": "Project Orion (nuclear propulsion)",
+        "url": "https://en.wikipedia.org/wiki/Project_Orion_(nuclear_propulsion)",
+        "why_interesting": "Bsically, you can make rocket using nukes and they are pretty efficient",
+        "additional_urls": ""
+    },
+    {
+        "name": "Solifugy",
+        "url": "https://cs.wikipedia.org/wiki/Solifugy",
+        "why_interesting": "cool arachnides I didin't know existed",
+        "additional_urls": ""
+    },
+    {
+        "name": "Anti-predator adaptation",
+        "url": "https://en.wikipedia.org/wiki/Anti-predator_adaptation",
+        "why_interesting": "Amazing adaptations of animals to avoid being eaten",
+        "additional_urls": ""
+    },
+    {
+        "name": "Svijonožci",
+        "url": "https://cs.wikipedia.org/wiki/Svijono%C5%BEci",
+        "why_interesting": "They got the longest pp",
+        "additional_urls": [
+            {
+                "name": "Deep look video",
+                "url": "https://www.youtube.com/watch?v=lfnkFxg1gMw"
+            }
+        ]
+    },
+
+    {
+        "name": "Naegleria fowleri",
+        "url": "https://cs.wikipedia.org/wiki/Naegleria_fowleri",
+        "why_interesting": "Brain eating amoeba. They lives in warm water, it is very rary but very deadly. Biggest epidemy was in Ústí nad Labem, 16 young people died after swimming in swimming pool.",
+        "additional_urls": [
+            {
+                "name": "Kurzgezagt video",
+                "url": "https://www.youtube.com/watch?v=7OPg-ksxZ4Y"
+            }
+        ]
+    },
+    {
+        "name": "Earthquake light",
+        "url": "https://en.wikipedia.org/wiki/Earthquake_light",
+        "why_interesting": "Strange lights that appear during earthquakes",
+        "additional_urls": ""
+    },
+    {
+        "name": "Crash at Crush",
+        "url": "https://en.wikipedia.org/wiki/Crash_at_Crush",
+        "why_interesting": "haha trains goes brrrrr",
+        "additional_urls": ""
+    },
+    {
+        "name": "Látky vzbuzující mimořádné obavy",
+        "url": "https://cs.wikipedia.org/wiki/L%C3%A1tky_vzbuzuj%C3%ADc%C3%AD_mimo%C5%99%C3%A1dn%C3%A9_obavy",
+        "why_interesting": "Just some list of dangerous chemicals",
+        "additional_urls": ""
+    },
+    {
+        "name": "Pralesnička strašná",
+        "url": "https://cs.wikipedia.org/wiki/Pralesni%C4%8Dka_stra%C5%A1n%C3%A1",
+        "why_interesting": "These frogs have one of the strongest poison in the world. Their only predators are humans which uses their poison for hunting and snakes which are resistant to their poison (not immune). They are also pretty inteligent (for a frog).",
+        "additional_urls": ""
+    },
+    {
+        "name": "Hans-Ulrich Rudel",
+        "url": "https://cs.wikipedia.org/wiki/Hans-Ulrich_Rudel",
+        "why_interesting": "",
+        "additional_urls": ""
+    },
+    {
+        "name": "Fainting goat",
+        "url": "https://en.wikipedia.org/wiki/Fainting_goat",
+        "why_interesting": "",
+        "additional_urls": ""
+    },
+    {
+        "name": "Sonoluminescence",
+        "url": "https://en.wikipedia.org/wiki/Sonoluminescence",
+        "why_interesting": "",
+        "additional_urls": ""
+    },
+
+
+
+
+    {
+        "name": "Cotylorhynchus",
+        "url": "https://en.wikipedia.org/wiki/Cotylorhynchus",
+        "why_interesting": "",
+        "additional_urls": ""
+    },
+    {
+        "name": "Vasilij Blochin",
+        "url": "https://cs.wikipedia.org/wiki/Vasilij_Blochin",
+        "why_interesting": "",
+        "additional_urls": ""
+    },
+    {
+        "name": "Jiraiya",
+        "url": "https://en.wikipedia.org/wiki/Jiraiya",
+        "why_interesting": "",
+        "additional_urls": ""
+    },
+    {
+        "name": "Jacob sheep",
+        "url": "https://en.wikipedia.org/wiki/Jacob_sheep",
+        "why_interesting": "",
+        "additional_urls": ""
+    },
+    {
+        "name": "Spectral color",
+        "url": "https://en.wikipedia.org/wiki/Spectral_color#Extra-spectral_colors",
+        "why_interesting": "",
+        "additional_urls": ""
+    },
+    {
+        "name": "Neotenie",
+        "url": "https://cs.wikipedia.org/wiki/Neotenie",
+        "why_interesting": "",
+        "additional_urls": ""
+    },
+    {
+        "name": "Ferrocerium",
+        "url": "https://en.wikipedia.org/wiki/Ferrocerium#:~:text=Ferrocerium%20(also%20known%20in%20Europe,of%20iron%20and%20%2F%20or%20magnesium.",
+        "why_interesting": "",
+        "additional_urls": ""
+    },
+    {
+        "name": "Marvin Heemeyer",
+        "url": "https://en.wikipedia.org/wiki/Marvin_Heemeyer",
+        "why_interesting": "",
+        "additional_urls": ""
+    },
+    {
+        "name": "Lepovkovití",
+        "url": "https://cs.wikipedia.org/wiki/Lepovkovit%C3%AD",
+        "why_interesting": "",
+        "additional_urls": ""
+    },
+    {
+        "name": "Jack Churchill",
+        "url": "https://en.wikipedia.org/wiki/Jack_Churchill",
+        "why_interesting": "",
+        "additional_urls": ""
+    },
+    {
+        "name": "Tarrare",
+        "url": "https://en.wikipedia.org/wiki/Tarrare",
+        "why_interesting": "",
+        "additional_urls": ""
+    },
+    {
+        "name": "List of longest-living organisms",
+        "url": "https://en.wikipedia.org/wiki/List_of_longest-living_organisms",
+        "why_interesting": "",
+        "additional_urls": ""
+    },
+    {
+        "name": "Otto Lilienthal",
+        "url": "https://cs.wikipedia.org/wiki/Otto_Lilienthal",
+        "why_interesting": "",
+        "additional_urls": ""
+    },
+    {
+        "name": "Sonderkommando Elbe",
+        "url": "https://en.wikipedia.org/wiki/Sonderkommando_Elbe",
+        "why_interesting": "",
+        "additional_urls": ""
+    },
+    {
+        "name": "Hitch (knot)",
+        "url": "https://en.wikipedia.org/wiki/Hitch_(knot)",
+        "why_interesting": "",
+        "additional_urls": ""
+    }
+
+
+
+
+
+
+
+
+
+]`;
+
+YearZero = 2024;
+WeekZero = 19;
+const objs = JSON.parse(jsonString);
+
+
+function getDateWeek(date) {
+    const currentDate = 
+        (typeof date === 'object') ? date : new Date();
+    const januaryFirst = 
+        new Date(currentDate.getFullYear(), 0, 1);
+    const daysToNextMonday = 
+        (januaryFirst.getDay() === 1) ? 0 : 
+        (7 - januaryFirst.getDay()) % 7;
+    const nextMonday = 
+        new Date(currentDate.getFullYear(), 0, 
+        januaryFirst.getDate() + daysToNextMonday);
+ 
+    return (currentDate < nextMonday) ? 52 : 
+    (currentDate > nextMonday ? Math.ceil(
+    (currentDate - nextMonday) / (24 * 3600 * 1000) / 7) : 1);
+}
+
+const today1 = new Date();
+const weekNumber = getDateWeek(today1);
+const year = today1.getFullYear();
+FactIndex = (year - YearZero) * 52 + weekNumber - 3;
+
+
+document.getElementById("FunFact").innerHTML = "<a href='"+objs[FactIndex]["url"]+"'>"+objs[FactIndex]["name"]+"</a>";
+
+
+const pastFunFacts = document.querySelector(".PastFunFacts tbody");
+index = 0;
+objs.slice(0, FactIndex).reverse().forEach((obj, index) => {
+    const row = document.createElement("tr");
+    const sourceCell = document.createElement("td");
+    const factCell = document.createElement("td");
+    const linkCell = document.createElement("td");
+    const link = document.createElement("a");
+
+    const anchor = document.createElement("a");
+    anchor.href = obj["url"];
+    anchor.textContent = obj["name"];
+    sourceCell.appendChild(anchor);
+    factCell.textContent = `${obj["why_interesting"]}`;
+    // link.href = obj["url"];
+    // link.textContent = "Link";
+    if (obj["additional_urls"]) {
+        const additionalLinks = obj["additional_urls"];
+        additionalLinks.forEach((nameurl) => {
+            name = nameurl["name"];
+            url = nameurl["url"];
+            const additionalLink = document.createElement("a");
+            additionalLink.href = url;
+            additionalLink.textContent = name;
+            link.appendChild(document.createElement("br"));
+            link.appendChild(additionalLink);
+        });
+    }
+
+    linkCell.appendChild(link);
+    row.appendChild(sourceCell);
+    row.appendChild(factCell);
+    row.appendChild(linkCell);
+    pastFunFacts.appendChild(row);
+});
+
+window.setInterval(
+    () => {
+        IsClass = false;
+        if (!IsClass){
+            document.getElementById("Time").innerHTML = "ZKOUŠKYYYYYYYYYYYYYYYYYY";
+            document.getElementById("ClasName").innerHTML = "";
+        }
+        
+
+    }
+);
